48e35627aaa162b307fdee630cfa9410dd510ddd
MrAppleGuy <67973830+MrAppleGuy@users.noreply.github.com>
2025-03-31 20:11:06 -0700

    Update fhn_time_and_phase.R

--- FHN Models/fhn_time_and_phase.R
+++ FHN Models/fhn_time_and_phase.R
@@ -1,80 +1,54 @@
-#
-# numerically solve FitzHugh-Nagumo equations
-#    dv/dt = r*v*(1-v/K)*(v/a-1) - b*w + I0
-#    dw/dt = ep*(v-w)
-#
-#  plot the solution as a function of time and in the phase plane including null clines
-#
+rm(list=ls())
 
-# set parameters
-# --------------
-dt     = 0.1        # time step
-Nt     = 5000        # number of time steps
+# Parameters
+dt <- 0.1
+Nt <- 5000
+r  <- 0.1
+K  <- 1.0
+a  <- 0.1
+I0 <- 0.0
+b  <- 1
+ep <- 0.005
+v0 <- 0.2
 
-r     = 0.1         # low v decay rate
-K     = 1.0         # carrying capacity
-a     = 0.1         # threshold
-v0    = 0.2      # initial condition
+# Define model functions
+f_v <- function(v, w) {
+  r * v * (1 - v/K) * (v/a - 1) - b * w + I0
+}
+f_w <- function(v, w) {
+  ep * (v - w)
+}
 
-ep    = 0.005        # recovery rate (inverse time scale)
-b     = 1           # rate w lowers v
-I0    = 0.0          # input current
+# Initialize time series
+v <- numeric(Nt + 1)
+w <- numeric(Nt + 1)
+t <- seq(0, Nt*dt, by = dt)
+v[1] <- v0
+w[1] <- 0  # assume initial w = 0
 
-#  dv/dt = fv(v,w)
-#  dw/dt = fw(v,w)
-#
-fv=function(v,w) r*v*(1-v/K)*(v/a-1) - b*w + I0
-fw=function(v,w) ep*(v-w)
+# Time evolution
+for (n in 1:Nt) {
+  v[n+1] <- v[n] + dt * f_v(v[n], w[n])
+  w[n+1] <- w[n] + dt * f_w(v[n], w[n])
+}
 
-# initialize storage for solution and time vector
-#--------------------------------
-v   = numeric(Nt+1)
-w   = numeric(Nt+1)
-t   = (0:Nt)*dt
+# Plot time series
+par(mfrow = c(1,2))
+plot(t, v, type = 'l', col = 'blue', lwd = 2,
+     xlab = "Time", ylab = "v", main = "v over Time")
+lines(t, w, col = 'red', lwd = 2)
+legend("topright", legend = c("v", "w"), col = c("blue", "red"), lty = 1)
 
-# set initial conditions
-# ----------------------
-v[1] = v0
+# Phase plane with nullclines
+vv <- seq(-0.4, 1.2, by = 0.01)
+# v-nullcline: dv/dt = 0 => w = [r*v*(1-v/K)*(v/a-1) + I0] / b
+w_nullcline <- r * vv * (1 - vv/K) * (vv/a - 1) / b + I0/b
+w_identity <- vv  # from dw/dt = ep*(v-w)=0 => v = w
 
-# run simulation
-# --------------
-
-for (n in 1:Nt){
-  
-  # update
-  #
-  v[n+1] = v[n] + dt*fv(v[n],w[n])
-  w[n+1] = w[n] + dt*fw(v[n],w[n])
-  
-}  # end loop in time
-
-
-par(mfrow=c(1,2))
-
-# plot the solution
-#------------------
-plot(t,v,xlab='time',type='l',col='blue',lwd=2)
-lines(t,w,col='red',lwd=3)
-
-
-
-# plot the nullclines in the phase plane
-#
-vv=seq(from=-0.4,to=1.2,by=0.01)
-vdot0 = r*vv*(1-vv)*(vv/a-1)/b
-wdot0 = vv
-
-# plot v nullcline
-#
-plot(vv,vdot0,type='l',xlim=c(-0.4,1.2),ylim=c(-0.05,0.2),col='cyan3',xlab='v',ylab='w',lwd=3,lty=5)
-
-# plot w nullcline
-#
-lines(vv,wdot0,lty=5,col='coral3',lwd=3)
-
-# plot the solution in phase space
-#
-lines(v,w,col='black',lwd=2)
-
-
-
+plot(vv, w_nullcline, type = 'l', lty = 5, lwd = 3, col = 'cyan3',
+     xlab = "v", ylab = "w", xlim = c(-0.4, 1.2), ylim = c(-0.05, 0.2),
+     main = "Phase Plane & Nullclines")
+lines(vv, w_identity, lty = 5, lwd = 3, col = 'coral3')
+lines(v, w, col = 'black', lwd = 2)
+legend("topright", legend = c("v-nullcline", "w-nullcline", "Trajectory"),
+       col = c("cyan3", "coral3", "black"), lty = c(5,5,1), lwd = c(3,3,2))
